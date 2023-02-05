6f532710c7f57b0cad1e5770b33c05ec6a90662b
Armando Murillo <totovip1464@gmail.com>
2023-02-04 20:13:14 -0600

    Refactor

--- src/utils/getTurnRandomCPU.ts
+++ src/utils/getTurnRandomCPU.ts
@@ -1,5 +1,10 @@
 import { winConditions } from "../services/winConditions";
 
+//La siguiente funciónd detecta si el usuario tiene una posibilidad de ganar
+//Ya que según las 'winConditions' él detecta si el usuario tiene más de dos equiz a su favor
+//De ser así devuelve true, para que de esta manera el próximo movimiento de la máquina no sea random
+//sino que será inteligente y pondrá una ficha en el lugar que puede evitar el gane del usuario.
+
 export const getTurnRandomCPU = (state : string[]) => {
     return winConditions.some((item, i) => {
         let [a, b, c] = winConditions[i];
